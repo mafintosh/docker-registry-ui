8e4c62e3e361b37bc61baced3f0e8f1f428fd76b
Mathias Buus <mathiasbuus@gmail.com>
2014-10-15 17:04:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# docker-registry-ui
+
+ui for [docker-registry-server](https://github.com/mafintosh/docker-registry-server). wip
+
+```
+# start local registry
+npm run dev
+open index.html
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+var xhr = require('xhr')
+var tree = require('tree-view')
+var highlight = require('highlight.js')
+
+var browser = tree()
+var image = window.location.hash.slice(1) || 'fe4f71a4d666f85558a9fd638b6e3482b44ff25c367724a029011e6de86f09bb'
+
+var onerror = function(err) {
+  throw err
+}
+
+var cd = function(p) {
+  xhr({
+    method: 'GET',
+    url: 'http://localhost:8000/v1/images/'+image+'/tree'+p,
+    json: true
+  }, function(err, response) {
+    if (err) return onerror(err)
+    browser.directory(p, response.body)
+  })
+}
+
+var cat = function(p, image) {
+  xhr({
+    method: 'GET',
+    url: 'http://localhost:8000/v1/images/'+image+'/blobs'+p
+  }, function(err, response) {
+    if (err) return onerror(err)
+
+    var $code = document.querySelector('#code-view code')
+    $code.className = 'hljs'
+    $code.innerHTML = response.body
+    highlight.highlightBlock($code)
+  })
+}
+
+browser.on('directory', function(p) {
+  cd(p)
+})
+
+browser.on('file', function(p, entry) {
+  cat(p, entry.image)
+})
+
+browser.appendTo('#tree-view')
+cd('/')
